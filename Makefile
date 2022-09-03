a4c528f8daf97e4ee67c9bf0819ff74a33df1ecc
Miguel <sv.mliano@hotmail.com>
2022-09-03 10:49:52 +0200

    New program random.c

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ test:
 info: common.h
 	$(CC) $(CFLAGS) info.c common.c -o info -L/usr/lib/ -lcryptoauth -I/usr/include/cryptoauthlib/
 
+random: common.h
+	$(CC) $(CFLAGS) random.c common.c -o random -L/usr/lib/ -lcryptoauth -I/usr/include/cryptoauthlib/
+
 read_write: common.h
 	$(CC) $(CFLAGS) read_write.c common.c -o read_write -L/usr/lib/ -lcryptoauth -I/usr/include/cryptoauthlib/
 
@@ -33,6 +36,6 @@ config: common.h
 	$(CC) $(CFLAGS) config.c common.c -o config -L/usr/lib/ -lcryptoauth -I/usr/include/cryptoauthlib/
 
 clean:
-	rm -f test info config read_write asymmetric symmetric sign_verify pbkdf2 generate_hash
+	rm -f test info config read_write asymmetric symmetric sign_verify pbkdf2 generate_hash random
 	rm -f *.o
 	rm -rf dec.txt enc.txt
---
+++ random.c
@@ -0,0 +1,37 @@
+#include "common.h"
+
+#define NUMBER_SIZE 32
+
+/* This program generates random numbers and displays them in decimal/hexadecimal format */
+int main (int argc, char** argv)
+{
+    ATCA_STATUS status;
+    uint8_t rand_out[NUMBER_SIZE];
+    ATCAIfaceCfg *gCfg = &cfg_ateccx08a_i2c_default;
+
+    gCfg->atcai2c.bus=1;
+
+    /* Creates and initializes ATCADevice context */
+    status = atcab_init(gCfg);
+    if (status != ATCA_SUCCESS)
+    {
+        fprintf(stderr, "Error initializing global ATCADevice\n", status);
+        exit(status);
+    }
+
+    /* Generates 32 byte random number from the device */
+    status = atcab_random(rand_out);
+    if (status != ATCA_SUCCESS)
+    {
+        fprintf(stderr, "Error generating random number\n");
+	return -1;
+    }
+
+    fprintf(stdout, "Random number generated succesfully!");
+    fprintf(stdout, "Decimal format: %d\n", rand_out);
+
+    fprintf(stdout, "Hexadecimal format: ");
+    print_hex(rand_out, NUMBER_SIZE);
+
+    return 0;
+}
