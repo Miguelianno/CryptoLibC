9bdf619d6a79a7246641aa044b365b16145064ee
Miguelianno <37307087+Miguelianno@users.noreply.github.com>
2022-09-05 14:44:10 +0200

    Initiating Execution_test script

--- test/execution_test.sh
+++ test/execution_test.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+
+# Testing read_write program
+
+# Testing random program
+
+# Testing generate_hash program
+
+# Testing asymmetric program
+
+# Testing symmetric program
+
+# Testing pbkdf2 program
+
+# Testing sign_verify program
